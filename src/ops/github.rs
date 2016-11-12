0b45eefddbaddcf4abae51e0592add52960d712d
nabijaczleweli <nabijaczleweli@gmail.com>
2016-11-12 22:15:00 +0100

    Add documentation for ops::init and ops::github

--- src/ops/github.rs
+++ src/ops/github.rs
@@ -1,37 +1,168 @@
+//! This module contains functions which interface with the GitHub API.
+//!
+//! Every function in this module needs an instance of `AppTokens`, wherein the `discord` field does *not* need to be set.
+//!
+//! The GitHub authentication is used just to get a bigger rate limit,
+//! so if you don't need to make a lot of requests just pass an empty string.
+
+
 use hyper::header::{Authorization, IfNoneMatch, EntityTag, UserAgent, Bearer, ETag};
 use self::super::super::util::GITHUB_USER_AGENT;
 use hyper::status::StatusCode;
 use self::super::super::Error;
 use self::super::AppTokens;
+use self::headers::*;
 use hyper::Client;
 use std::io::Read;
 
 
-header! {
-    (XPollInterval, "X-Poll-Interval") => [u64]
+mod headers {
+    header! {
+        (XPollInterval, "X-Poll-Interval") => [u64]
+    }
 }
 
 
+/// Check whether a user with the specified name exists.
+///
+/// # Examples
+///
+/// ```
+/// # use dishub::ops::AppTokens;
+/// # use dishub::ops::github::user_exists;
+/// # let tokens = AppTokens {
+/// #     github: "".to_string(),
+/// #     discord: "".to_string(),
+/// # };
+/// let response = user_exists("nabijaczleweli", &tokens);
+/// assert_eq!(response, Ok(true));
+/// ```
 pub fn user_exists(uname: &str, tokens: &AppTokens) -> Result<bool, Error> {
     exists(format!("https://api.github.com/users/{}", uname), tokens, "GitHub user information")
 }
 
+/// Check whether a repository with the specified slug exists.
+///
+/// # Examples
+///
+/// ```
+/// # use dishub::ops::AppTokens;
+/// # use dishub::ops::github::repo_exists;
+/// # let tokens = AppTokens {
+/// #     github: "".to_string(),
+/// #     discord: "".to_string(),
+/// # };
+/// let response = repo_exists("nabijaczleweli/dishub", &tokens);
+/// assert_eq!(response, Ok(true));
+/// ```
 pub fn repo_exists(slug: &str, tokens: &AppTokens) -> Result<bool, Error> {
     exists(format!("https://api.github.com/repos/{}", slug), tokens, "GitHub repository")
 }
 
+/// Get the events for a user when you don't have an ETag (which is to say - for the first time).
+///
+/// The returned tuple contains:
+///
+///   * The raw JSON response,
+///   * The event bundle's ETag,
+///   * The next minimum amount of milliseconds polling the same event queue is permitted.
+///
+/// You should use this only once and use `poll_user_events_update()` afterwards.
+///
+/// # Examples
+///
+/// ```no_run
+/// # use dishub::ops::AppTokens;
+/// # use dishub::ops::github::poll_user_events_new;
+/// # let tokens = AppTokens {
+/// #     github: "".to_string(),
+/// #     discord: "".to_string(),
+/// # };
+/// let (response, etag, next) = poll_user_events_new("nabijaczleweli", &tokens).unwrap();
+/// ```
 pub fn poll_user_events_new(uname: &str, tokens: &AppTokens) -> Result<(String, String, u64), Error> {
     poll_events_new(format!("https://api.github.com/users/{}/events", uname), tokens, "GitHub user events")
 }
 
+/// Get the events for a repository when you don't have an ETag (which is to say - for the first time).
+///
+/// The returned tuple contains:
+///
+///   * The raw JSON response,
+///   * The event bundle's ETag,
+///   * The next minimum amount of milliseconds polling the same event queue is permitted.
+///
+/// You should use this only once and use `poll_repo_events_update()` afterwards.
+///
+/// # Examples
+///
+/// ```no_run
+/// # use dishub::ops::AppTokens;
+/// # use dishub::ops::github::poll_repo_events_new;
+/// # let tokens = AppTokens {
+/// #     github: "".to_string(),
+/// #     discord: "".to_string(),
+/// # };
+/// let (response, etag, next) = poll_repo_events_new("nabijaczleweli/dishub", &tokens).unwrap();
+/// ```
 pub fn poll_repo_events_new(slug: &str, tokens: &AppTokens) -> Result<(String, String, u64), Error> {
     poll_events_new(format!("https://api.github.com/repos/{}/events", slug), tokens, "GitHub repo events")
 }
 
+/// Get the events for a user when you already have an ETag (which is to say - after the first time).
+///
+/// If the event list hasn't changed the first element of the returned tuple will be `None`,
+/// otherwise it's a tuple of:
+///
+///   * The raw JSON response,
+///   * The event bundle's new ETag.
+///
+/// The second element always constains the next minimum amount of milliseconds polling the same event queue is permitted.
+///
+/// # Examples
+///
+/// ```no_run
+/// # use dishub::ops::AppTokens;
+/// # use dishub::ops::github::poll_user_events_update;
+/// # let tokens = AppTokens {
+/// #     github: "".to_string(),
+/// #     discord: "".to_string(),
+/// # };
+/// # let prev_etag = "9c1bac04e0735a8cba6a7b277b70c19f";
+/// let (changed, next) = poll_user_events_update("nabijaczleweli", prev_etag, &tokens).unwrap();
+/// if let Some((response, etag)) = changed {
+///     // The feed changed
+/// }
+/// ```
 pub fn poll_user_events_update(uname: &str, e_tag: &str, tokens: &AppTokens) -> Result<(Option<(String, String)>, u64), Error> {
     poll_events_update(format!("https://api.github.com/users/{}/events", uname), e_tag, tokens, "GitHub user events")
 }
 
+/// Get the events for a repository when you already have an ETag (which is to say - after the first time).
+///
+/// If the event list hasn't changed the first element of the returned tuple will be `None`,
+/// otherwise it's a tuple of:
+///
+///   * The raw JSON response,
+///   * The event bundle's new ETag.
+///
+/// The second element always constains the next minimum amount of milliseconds polling the same event queue is permitted.
+///
+/// # Examples
+///
+/// ```no_run
+/// # use dishub::ops::AppTokens;
+/// # use dishub::ops::github::poll_repo_events_update;
+/// # let tokens = AppTokens {
+/// #     github: "".to_string(),
+/// #     discord: "".to_string(),
+/// # };
+/// # let prev_etag = "4797f0ad2ee145181045fe69c61676e6";
+/// let (changed, next) = poll_repo_events_update("nabijaczleweli/dishub", prev_etag, &tokens).unwrap();
+/// if let Some((response, etag)) = changed {
+///     // The feed changed
+/// }
+/// ```
 pub fn poll_repo_events_update(slug: &str, e_tag: &str, tokens: &AppTokens) -> Result<(Option<(String, String)>, u64), Error> {
     poll_events_update(format!("https://api.github.com/repos/{}/events", slug), e_tag, tokens, "GitHub user events")
 }
--- src/ops/init.rs
+++ src/ops/init.rs
@@ -1,3 +1,15 @@
+//! This module contains the functions used only by the `init` subsystem.
+//!
+//! The flow of the `init` subsystem is as follows:
+//!
+//! ```plaintext
+//! Options::parse()
+//! |> ops::init::verify()
+//! |> ops::init::get_data()
+//! |> ops::AppTokens::write()
+//! ```
+
+
 use self::super::super::util::prompt_exact_len;
 use self::super::{AppTokens, verify_file};
 use self::super::super::Error;
@@ -5,10 +17,57 @@ use std::io::{BufRead, Write};
 use std::path::PathBuf;
 
 
+/// Verify if, given the current configuration, it's permitted to continue with the subsequent steps of the `init` subsystem.
+///
+/// The return value contains either the path to the file containing the global app tokens or why getting it failed.
+///
+/// # Examples
+///
+/// Verifying a nonexistant file or an existing file with forcing.
+///
+/// ```
+/// # use std::env::temp_dir;
+/// # use dishub::ops::init;
+/// let tf = temp_dir().join("dishub-doctest").join("ops-init-verify-0");
+/// assert_eq!(init::verify(&("$TEMP/ops-init-verify-0".to_string(), tf.clone()), true),
+///            Ok(tf.join("tokens.toml")));
+/// ```
+///
+/// Verifying an existing file without forcing.
+///
+/// ```
+/// # use std::fs::{self, File};
+/// # use std::env::temp_dir;
+/// # use dishub::ops::init;
+/// # use dishub::Error;
+/// # use std::io::Write;
+/// let tf = temp_dir().join("dishub-doctest").join("ops-init-verify-1");
+/// fs::create_dir_all(&tf).unwrap();
+/// File::create(tf.join("tokens.toml")).unwrap().write(&[]).unwrap();
+///
+/// assert_eq!(init::verify(&("$TEMP/ops-init-verify-1".to_string(), tf), false),
+///            Err(Error::OverrideNoForce("$TEMP/ops-init-verify-1/tokens.toml".to_string())));
+/// ```
 pub fn verify(config_dir: &(String, PathBuf), force: bool) -> Result<PathBuf, Error> {
     verify_file("tokens.toml", false, config_dir, force, "")
 }
 
+/// Prompt the user for application data.
+///
+/// # Examples
+///
+/// ```
+/// # use dishub::ops::{init, AppTokens};
+/// # use std::io::BufReader;
+/// assert_eq!(init::get_data(
+///     &mut BufReader::new(&b"994c365aec1700f5783bac697e2347ffd2268e1e\n\
+///                            a8233f2465e4e27b36c3e9a9.5ec9ab.e8fe135112b5f4d678bd1d221f8\n"[..]),
+///     &mut Vec::new()),
+///     AppTokens {
+///         github: "994c365aec1700f5783bac697e2347ffd2268e1e".to_string(),
+///         discord: "a8233f2465e4e27b36c3e9a9.5ec9ab.e8fe135112b5f4d678bd1d221f8".to_string(),
+///     });
+/// ```
 pub fn get_data<R: BufRead, W: Write>(input: &mut R, output: &mut W) -> AppTokens {
     AppTokens {
         github: prompt_exact_len(input, output, "GitHub OAuth token", |_| true, 40).unwrap(),
